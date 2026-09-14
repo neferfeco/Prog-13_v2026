68cf5cb2bacdfa3465d3ec5574027f92808a2092
Mona Lisa <fpiller@gmail.com>
2026-09-14 09:41:12 +0200

    2026-09-14

--- szamlakezelo_20260908.py
+++ szamlakezelo_20260908.py
@@ -9,8 +9,29 @@ adatfajl = "szamla.txt"
 jogosult = False
 
 
-# MŰKÖDÉS
-# BELÉPÉS
+#######################################
+# FUNKCIÓK
+#######################################
+def egyenleg():
+    print("Az egyenleged: ")
+
+def utalas(osszeg):
+    print("Utalás: ")
+
+def penzbetet(osszeg):
+    print("Betét: ")
+
+# darab = 0 -> összes tranzakció
+# darab !=0 -> utolsó darab tranzakcio
+def tortenet(darab):
+    print("Tranzakciók: ")
+
+#######################################
+# FUNKCIÓK VÉGE
+#######################################
+
+
+# BEJELENTKEZÉS
 hibas_belepesszam = 3
 
 pk = int(input("Add meg a PIN kódodat!: "))
@@ -19,8 +40,8 @@ if pk == pin_kod:
         jogosult = True
         print("Sikeres belépés!")
 
-while(not jogosult and hibas_belepesszam > 0):
-    print("Hibás PIN kód!")
+while(not jogosult and hibas_belepesszam > 1):
+    print(f"Hibás PIN kód!")
     pk = int(input("Add meg a PIN kódodat!: "))
     hibas_belepesszam -= 1
   
@@ -28,12 +49,49 @@ while(not jogosult and hibas_belepesszam > 0):
         jogosult = True
         print("Sikeres belépés!")
 
+if not jogosult:
+    print(f"Hibás PIN kód!")
+
+
+# FUNKCIÓVÁLASZTÓ MENÜ
+cim = "\nSZÁMLAKEZELŐ PROGRAM\n====================\n"
+menu = [
+    "1. Egyenleg lekérdezés",
+    "2. Pénz kivétel/átutalás",
+    "3. Pénz betét",
+    "--------------",
+    "4. Tranzakciótörténet",
+    "9. Kilépés"
+]
+
+menupontok = [1, 2, 3, 4, 9]
+
+print(cim)
+for me in menu:
+    print(f"{me}")
+
+valasztas = int(input("Válassz tevékenységet: "))
+
+while valasztas not in menupontok:
+    print("Nincs ilyen menüpont!\n")
     
-    
+    print(cim)    
+    for me in menu:
+        print(f"{me}")
+
+    valasztas = int(input("Válassz tevékenységet: "))
 
 
-
-
+if valasztas == 1:
+    egyenleg()
+elif valasztas == 2:
+    utalas(123)
+elif valasztas == 3:
+    penzbetet(10000)
+elif valasztas == 4:
+    tortenet(0)
+elif valasztas == 9:
+    exit()
 
 
 
